8c57f2d6851170a7b5686150a157b58e351247cc
Airah Sophia Japzon <airahsophia13@gmail.com>
2024-10-18 16:23:35 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+posts = [
+    {
+        'author': 'Airah Sophia',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 20, 2024'
+    },
+    {
+        'author': 'Miyuki',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 26, 2024'
+
+    },
+    {
+        'author': 'Jaci Mae Diaz',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'September 29, 2024'
+    }
+
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    #return HttpResponse('<h1> WELCOME </h1>')
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    #return HttpResponse('<h1> ABOUT </h1>')
+    return render(request, 'blog/about.html')
